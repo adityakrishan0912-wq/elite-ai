e4ad0d8be84f24934def4a69899ce3f4415fcbbe
adityakrishan0912-wq <adityakrishan0912@gmail.com>
2026-08-13 18:24:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ if not api_key:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
+
 
 uploaded_file = st.file_uploader("📷 फोटो अपलोड करें", type=["jpg", "jpeg", "png"])
 
